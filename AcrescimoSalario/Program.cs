fa1b64826b599c5d22b9be470953af70e5da1c51
veronezzi159 <lucasveronezzi3@gmail.com>
2024-04-21 16:19:15 -0300

    First Commit

--- AcrescimoSalario/Program.cs
+++ AcrescimoSalario/Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿decimal salario, acrescimo, resultado;
+do
+{
+    Console.WriteLine("Digite o valor de seu salario");
+    salario = decimal.Parse(Console.ReadLine());
+    Console.WriteLine("Digite o acrscimo que deseja(sem o %)");
+    acrescimo = decimal.Parse(Console.ReadLine());
+    if (acrescimo <= 0 || salario <= 0 )
+    {
+        Console.WriteLine("Acrescimo ou salário zerados ou negatios, insira valores valídos");
+    }
+} while (salario <= 0 || acrescimo <= 0);
+
+resultado = salario + (salario * (acrescimo / 100));
+
+Console.WriteLine($"Seu salário com o valor acrescido irá totalizar R${resultado}");
