77b0bb656645dcb123ef80dae8a784065b1a5b71
veronezzi159 <lucasveronezzi3@gmail.com>
2024-04-22 10:16:32 -0300

    Correção de código

--- AcrescimoSalario/Program.cs
+++ AcrescimoSalario/Program.cs
@@ -1,16 +1,16 @@
-﻿decimal salario, acrescimo, resultado;
+﻿decimal salario, acrescimo, desejado;
 do
 {
     Console.WriteLine("Digite o valor de seu salario");
     salario = decimal.Parse(Console.ReadLine());
-    Console.WriteLine("Digite o acrscimo que deseja(sem o %)");
-    acrescimo = decimal.Parse(Console.ReadLine());
-    if (acrescimo <= 0 || salario <= 0 )
+    Console.WriteLine("Digite o salário desejado");
+    desejado = decimal.Parse(Console.ReadLine());
+    if (desejado <= 0 || salario <= 0 )
     {
         Console.WriteLine("Acrescimo ou salário zerados ou negatios, insira valores valídos");
     }
-} while (salario <= 0 || acrescimo <= 0);
+} while (salario <= 0 || desejado <= 0);
 
-resultado = salario + (salario * (acrescimo / 100));
+acrescimo = (desejado - salario) * 100/ salario;
 
-Console.WriteLine($"Seu salário com o valor acrescido irá totalizar R${resultado}");
+Console.WriteLine($"Para atingir o salário de {desejado}, você terá que obter um acréscimo de: {acrescimo}%");
